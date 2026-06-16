0955b0d1f389c9cc004860e36a10c8e682e8880c
Ádria Freitas <adrianadesenhos@gmail.com>
2026-06-16 11:12:51 -0300

    cria pagina mensalidades assinante

---
+++ src/app/cliente/[slug]/mensalidades/page.tsx
@@ -0,0 +1,12 @@
+export default function MensalidadesPage() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+        color: "#fff",
+      }}
+    >
+      💳 Minhas Mensalidades
+    </div>
+  );
+}
