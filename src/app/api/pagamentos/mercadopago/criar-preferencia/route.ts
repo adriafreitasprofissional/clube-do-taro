f717144e0a1077852df9526f547d2c13d74426b3
Ádria Freitas <adrianadesenhos@gmail.com>
2026-07-22 13:50:24 -0300

    Adiciona suporte a CORS OPTIONS

--- src/app/api/pagamentos/mercadopago/criar-preferencia/route.ts
+++ src/app/api/pagamentos/mercadopago/criar-preferencia/route.ts
@@ -6,10 +6,15 @@ const corsHeaders = {
   "Access-Control-Allow-Origin":
     "https://clube-do-taro-landing.vercel.app",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
 
 export async function POST(req: Request) {
   console.log("🔥 ENTREI NA API criar-preferencia");
