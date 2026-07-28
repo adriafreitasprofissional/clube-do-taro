7d3004ed3d839d34e6e617cedd646448d483a747
Ádria Freitas <adrianadesenhos@gmail.com>
2026-07-27 21:36:35 -0300

    Mundando o webhook final

--- src/lib/mercadopago/processarWebhook.ts
+++ src/lib/mercadopago/processarWebhook.ts
@@ -1,5 +1,6 @@
 import { Payment } from "mercadopago";
 import { mpClient } from "@/lib/mercadopago";
+import { supabaseAdmin } from "@/lib/supabase-admin";
 
 export async function processarWebhook(paymentId: string) {
   const payment = new Payment(mpClient);
@@ -7,6 +8,18 @@ export async function processarWebhook(paymentId: string) {
   const pagamento = await payment.get({
     id: paymentId,
   });
+
+  if (pagamento.status !== "approved") {
+    return pagamento;
+  }
+
+  const { data: cliente } = await supabaseAdmin
+    .from("club_clients")
+    .select("*")
+    .eq("email", pagamento.payer.email)
+    .maybeSingle();
+
+  console.log("CLIENTE:", cliente);
 
   console.log("=== PAGAMENTO ===");
   console.dir(pagamento, { depth: null });
